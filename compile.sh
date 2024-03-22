a791b51249d6eefbe5ffff95c7f1a502ec85047d
joyanta debnath <joydeb050@gmail.com>
2024-03-22 01:18:56 -0500

    Fixed library

--- compile.sh
+++ compile.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -e
 
-git submodule update --init
-cd src/ && make && cd ..
+git submodule update --init --remote
+cd agda-stdlib
+git checkout v1.7.1
+cd ..
+cd src/ && make all && cd ..
